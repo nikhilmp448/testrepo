2aacbf478e861776cfa10c0b9d9dc2404b250aa2
nikhil mp mp <nikhilmp448@gmail.com>
2023-09-30 20:16:41 +0530

    g

--- mooviz/settings.py
+++ mooviz/settings.py
@@ -53,11 +53,9 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'mooviz.urls'
 
+# Static files (CSS, JavaScript, images)
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 TEMPLATES = [
     {
