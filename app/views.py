c26598e8ef55aff2284b30013b39e84d6c92ded2
nikhil mp mp <nikhilmp448@gmail.com>
2023-10-01 13:07:02 +0530

    g

--- app/views.py
+++ app/views.py
@@ -4,43 +4,77 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_images():
+api_key = "fada996915db97d2c433bc87692e1fca"
 
+def get_popular():
+    url = "https://api.themoviedb.org/3/person/popular"
 
-# URL of the website with HD wallpapers
-    url = "https://www.hdwallpapers.in/cars-desktop-wallpapers.html"
+    params = {
+        "api_key": 'api_key',
+        "page": 1,  
+    }
 
-    # Define a user-agent to mimic a web browser request
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
-
-    # Send a GET request to the website with the custom headers
-    response = requests.get(url, headers=headers)
-
-    # Check if the request was successful
+    response = requests.get(url, params=params)
     if response.status_code == 200:
-        # Parse the HTML content of the page
-        soup = BeautifulSoup(response.content, "html.parser")
-
-        # Find all image tags with the "src" attribute
-        img_tags = soup.find_all("img", src=True)
-
-        # Extract the URLs from the "src" attribute of the image tags
-        image_urls = [img["src"] for img in img_tags]
-
-        # Filter the image URLs to include only those ending with ".jpg" (you can adjust this filter as needed)
-        hd_image_urls = [img_url for img_url in image_urls if img_url.endswith(".jpg")]
-
-        # Print the list of HD image URLs
-    #     for idx, img_url in enumerate(hd_image_urls, start=1):
-    #         print(f"HD Wallpaper {idx}: {img_url}")
-        return hd_image_urls
+        data = response.json()
+        return data
     else:
         return response.status_code
+    
+def get_trending_movies():
+    url = "https://api.themoviedb.org/3/trending/movie/day"
+    
+
+    params = {
+        "api_key": api_key,
+    }
+
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        trending_movies = response.json()
+        return trending_movies.get("results", [])
+    else:
+        return []
+    
+def get_trending_movie_posters():
+    trending_movies = get_trending_movies()
+
+    poster_urls = []
+    for movie in trending_movies:
+        poster_path = movie.get("poster_path")
+        if poster_path:
+            poster_url = f"https://image.tmdb.org/t/p/w200/{poster_path}"
+            poster_urls.append(poster_url)
+
+    return poster_urls
+
+def now_showing_movies():
+    url = "https://api.themoviedb.org/3/movie/now_playing"
+
+    params = {
+        "api_key": api_key,
+    }
+
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        now_showing_movies = response.json()
+        return now_showing_movies.get("results", [])
+    else:
+        return []
 
 
 
 def home(request):
-    images = get_images()
-    context = {'images':images}
+    popular = get_popular()
+    # trending = get_trending_movies()
+    now_streaming = now_showing_movies()
+    print(now_streaming)
+    trending_movie_poster = get_trending_movie_posters()
+    print(trending_movie_poster)
+
+    context = {'popular':popular,
+               'trending':trending_movie_poster
+               }
     return render(request,'index.html',context)
